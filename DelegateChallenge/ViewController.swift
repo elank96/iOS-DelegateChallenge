10222c1ca13fc17bbb678dbfb8962808245a0da1
Elan Kainen <elank96@gmail.com>
2019-12-04 22:16:36 -0500

    Initial Commit

---
+++ DelegateChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DelegateChallenge
+//
+//  Created by Elan Kainen on 12/4/19.
+//  Copyright © 2019 Elan Kainen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
